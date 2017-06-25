733c655d662e7eebae5a271d8263fbeecb65c93f
xafero <georg.berendt@uni-oldenburg.de>
2017-06-25 03:14:59 +0000

    Is configurable from outside

--- BlueBridgeJs/bridge.js
+++ BlueBridgeJs/bridge.js
@@ -1,14 +1,18 @@
 var winston = require('winston');
 require('winston-daily-rotate-file');
 
+var logLvl = process.env.LOG ? process.env.LOG : 'info';
+
 var fileTr = new winston.transports.DailyRotateFile({
    filename: 'bridge.log',
    datePattern: 'yyyy-MM-dd.',
    prepend: true,
-   level: process.env.ENV === 'development' ? 'debug' : 'info'
+   level: logLvl
 });
 
-var consTr = new (winston.transports.Console)();
+var consTr = new winston.transports.Console({
+   level: logLvl
+});
 
 var log = new (winston.Logger)({ transports: [consTr, fileTr] });
 
@@ -30,6 +34,17 @@ var onState = function (state)
 
 noble.on('stateChange', onState);
 
+var multiAddr = process.env.ADDR ? process.env.ADDR : '192.168.178.255';
+var multiPort = process.env.PORT ? process.env.PORT : 4242;
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+server.bind(multiPort, function(){
+    server.setBroadcast(true);
+    server.setMulticastTTL(128);
+});
+
+log.info('I will post to '+multiAddr+'/'+multiPort+' and log is '+logLvl+'!');
+
 var onFound = function (peripheral) 
 {
    var advertise = { 
@@ -45,8 +60,10 @@ var onFound = function (peripheral)
       ManufacturerData : peripheral.advertisement.manufacturerData.toString('hex'),
       PowerLevel : peripheral.advertisement.txPowerLevel
    };
-
-   console.log(JSON.stringify(advertise));
+   var json = JSON.stringify(advertise);
+   var message = new Buffer(json);
+   server.send(message, 0, message.length, multiPort, multiAddr);
+   log.debug('Sent ' + message.length + ' bytes => ' + json);
 };
 
 noble.on('discover', onFound);
