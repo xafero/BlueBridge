f0c72b0256ef40db6b714bdd36e4a87ec77d33b9
xafero <georg.berendt@uni-oldenburg.de>
2017-06-25 02:22:27 +0000

    Added logging to NodeJS

--- BlueBridgeJs/bridge.js
+++ BlueBridgeJs/bridge.js
@@ -1,8 +1,22 @@
+var winston = require('winston');
+require('winston-daily-rotate-file');
+
+var fileTr = new winston.transports.DailyRotateFile({
+   filename: 'bridge.log',
+   datePattern: 'yyyy-MM-dd.',
+   prepend: true,
+   level: process.env.ENV === 'development' ? 'debug' : 'info'
+});
+
+var consTr = new (winston.transports.Console)();
+
+var log = new (winston.Logger)({ transports: [consTr, fileTr] });
+
 var noble = require('noble');
 
 var onState = function (state) 
 {
-   console.log(state);
+   log.info('Bluetooth is now ' + state);
 }
 
 noble.on('stateChange', onState);
