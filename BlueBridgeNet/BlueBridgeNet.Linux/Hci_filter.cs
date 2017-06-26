e5c200d4f6c428d3f34798b9e4f7ed8024f0aa1c
ultra <ultra@devlinux.localdomain>
2017-06-26 21:15:14 +0200

    Added Linux impl

---
+++ BlueBridgeNet/BlueBridgeNet.Linux/Hci_filter.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BlueBridgeNet.Linux
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 2)]
+    struct Hci_filter
+    {
+        public uint type_mask;
+        public ulong event_mask;
+        public ushort opcode;
+    }
+}
--- BlueBridgeNet/BlueBridgeNet.Linux/LinuxBridge.cs
+++ BlueBridgeNet/BlueBridgeNet.Linux/LinuxBridge.cs
@@ -1,25 +1,83 @@
 ﻿using BlueBridgeNet.API;
+using log4net;
 using System;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace BlueBridgeNet.Linux
 {
     public class LinuxBridge : IBridge
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(LinuxBridge).Name);
+
         public event EventHandler<IBlueEvent> OnBlueEvent;
 
-        public void Dispose()
-        {
-            throw new NotImplementedException();
-        }
+        private bool shouldRun = true;
+        private Thread watcher;
 
         public void Start()
         {
-            throw new NotImplementedException();
+            log.Debug($"Hub initialising...");
+            var devId = Bluez.hci_get_route(IntPtr.Zero);
+            log.Debug($"Device ID = {devId}");
+            var sockFd = Linux.socket(Consts.AF_BLUETOOTH, Consts.SOCK_RAW | Consts.SOCK_CLOEXEC, Consts.BTPROTO_HCI);
+            log.Debug($"Socket FD = {sockFd}");
+            var addr = new Sockaddr_hci
+            {
+                hci_family = Consts.AF_BLUETOOTH,
+                hci_dev = (ushort)devId,
+                hci_channel = Consts.HCI_CHANNEL_RAW
+            };
+            var status = Linux.bind(sockFd, ref addr, Marshal.SizeOf(addr));
+            log.Debug($"Socket bind => {status}");
+            status = Bluez.hci_le_set_scan_parameters(sockFd, 0, 0x10, 0x10, 0, 0, 1000);
+            log.Debug($"Bluetooth LE scan parameters => {status}");
+            var filter = new Hci_filter
+            {
+                type_mask = 0x00000010,
+                event_mask = 0x4000000000000000ul,
+                opcode = 0
+            };
+            status = Linux.setsockopt(sockFd, Consts.SOL_HCI, Consts.HCI_FILTER, ref filter, Marshal.SizeOf(filter));
+            log.Debug($"Socket options => {status}");
+            status = Bluez.hci_le_set_scan_enable(sockFd, 1, 0, 1000);
+            log.Debug($"Bluetooth LE scan enable => {status}");
+            watcher = new Thread(() =>
+                        {
+                        while (shouldRun)
+                        {
+                            var bytes = new byte[44];
+#if !WINRT
+                                Mono.Unix.Native.Syscall.recv(sockFd, bytes, (ulong)bytes.Length, 0);
+#endif
+                                log.Debug($"Got {bytes.Length} bytes!");
+                                foreach (var raw in reader.Read(bytes, offset: 16))
+                                {
+                                    var parsed = (SimpleData)raw;
+                                    parsed.Time = DateTime.UtcNow;
+                                    OnHealthEvent?.Invoke(this, parsed);
+                                }
+                            }
+                        })
+            {
+                IsBackground = true,
+                Name = "Watcher"
+            };
+            watcher.Start();
+            log.Debug("Watcher started.");
         }
 
         public void Stop()
         {
-            throw new NotImplementedException();
+            log.Debug($"Hub disposing...");
+            shouldRun = false;
+            watcher.Interrupt();
+            log.Debug($"Watcher interrupted.");
+        }
+
+        public void Dispose()
+        {
+            Stop();
         }
     }
 }
